c95093600562062582f23a491f5766ef6c01c38b
Urszula Holodniak <uholodniak@gmail.com>
2018-10-23 17:40:57 +0200

    Fix controller test

--- Gemfile
+++ Gemfile
@@ -29,4 +29,5 @@ end
 
 group :test do
   gem 'faker'
+  gem 'rails-controller-testing'
 end
--- test/controllers/fire_brigades_controller_test.rb
+++ test/controllers/fire_brigades_controller_test.rb
@@ -1,8 +1,42 @@
 require 'test_helper'
 
 class FireBrigadesControllerTest < ActionDispatch::IntegrationTest
-  test 'should get index' do
-    get fire_brigades_index_url
+  Geocoder::Lookup::Test.add_stub(
+      "240 Broadway, New York", [
+      {
+          'coordinates'  => [40.712785, -74.006073],
+          'address'      => '240 Broadway, New York, USA',
+          'state'        => 'New York',
+          'state_code'   => 'NY',
+          'country'      => 'United States',
+          'country_code' => 'US'
+      }
+  ])
+
+  test 'should redirect to index action for FireBrigade Controller' do
+    get root_path
+
     assert_response :success
+    assert_template :index
+    assert_instance_of FireBrigadesController, @controller
+  end
+
+  test 'should return empty collection when not search params set' do
+    get root_path
+
+    assert_response :success
+    assert_template :index
+    assert_empty assigns(:fire_brigades)
+  end
+
+  test 'should return collection when search params set' do
+    fire_brigade = create(:fire_brigade, city: "New York", street: "90 Church St", latitude: 40.712655, longitude: -74.009914)
+    params = { city: "New York", street: "240 Broadway", radius: 1 }
+
+    get root_path, params: params
+
+    assert_response :success
+    assert_template :index
+    assert_equal [fire_brigade], assigns(:fire_brigades)
   end
 end
